d1bda76cfb2a3c965c5937c706fdd53336e5625e
Zhuravleva0701 <167981362+Zhuravleva0701@users.noreply.github.com>
2024-04-30 23:34:34 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,14 @@
+my_list = ['apple', 'orange', 'peach', 'banana', 'coconut', 'plum']
+print(my_list)
+print(my_list[0],my_list[3])
+print(my_list[2:-1])
+my_list[2] = 'apricot'
+print(my_list)
+
+
+my_dict = {'apple': 'яблоко', 'orange': 'апельсин', 'peach': 'персик'}
+print(my_dict)
+print(my_dict['apple'])
+my_dict['peach'] = 'ананас'
+my_dict['melon'] = 'дыня'
+print(my_dict)
